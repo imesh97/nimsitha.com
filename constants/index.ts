c131324bcc1e8ab83b56ee43275649f6e4cdf45e
imesh <imesh.nimsitha@gmail.ca>
2025-01-02 23:34:47 -0500

    created constants for file layout

---
+++ constants/index.ts
@@ -0,0 +1,13 @@
+export const sections = [
+  {
+    name: "portfolio",
+    type: "folder",
+    items: [
+      { name: "About.tsx", path: "/about" },
+      { name: "Experience.tsx", path: "/experience" },
+      { name: "Projects.tsx", path: "/projects" },
+      { name: "Skills.tsx", path: "/skills" },
+      { name: "Contact.tsx", path: "/contact" },
+    ],
+  },
+];
