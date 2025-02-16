78683fe9f7d03bdc2b15e468afacedc12133014d
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-02-16 04:24:18 -0500

    added 3xl breakpoint and accordion animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,29 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      screens: { "3xl": "1600px" },
     },
   },
   plugins: [require("tailwindcss-animate")],
