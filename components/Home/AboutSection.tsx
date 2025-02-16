6fcf02d98d2bf636dfd07e2df320239c80f45a84
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-02-16 16:11:01 -0500

    fixed styling with sidebar

--- components/Home/AboutSection.tsx
+++ components/Home/AboutSection.tsx
@@ -1,35 +1,48 @@
+"use client";
+
 import React from "react";
 import { Terminal, Coffee, Heart, Zap } from "lucide-react";
+import { useLayoutStore } from "@/store/layoutStore";
 
 const values = [
   {
-    icon: <Terminal className="w-6 h-6 text-blue-400" />,
     title: "detail-oriented",
     description: "A pixel-perfect approach to everything I do.",
+    icon: <Terminal className="w-6 h-6 text-blue-400" />,
+    color: "blue-400",
   },
   {
-    icon: <Coffee className="w-6 h-6 text-green-500" />,
     title: "problem solver",
     description: "Turning complex problems into elegant solutions.",
+    icon: <Coffee className="w-6 h-6 text-green-500" />,
+    color: "green-500",
   },
   {
-    icon: <Heart className="w-6 h-6 text-red-500" />,
     title: "user-focused",
     description: "Creating intuitive experiences for everyone.",
+    icon: <Heart className="w-6 h-6 text-red-500" />,
+    color: "red-500",
   },
   {
-    icon: <Zap className="w-6 h-6 text-yellow-500" />,
     title: "fast learner",
     description: "Adapting to new tech and best practices.",
+    icon: <Zap className="w-6 h-6 text-yellow-500" />,
+    color: "yellow-500",
   },
 ];
 
 export function AboutSection() {
+  const { sidebarOpen } = useLayoutStore();
+
   return (
     <div className="pt-20 pb-12">
       <div className="max-w-6xl mx-auto px-6">
-        <div className="grid md:grid-cols-2 gap-12">
-          <div className="space-y-6 content-center">
+        <div className="grid lg:grid-cols-2 gap-12">
+          <div
+            className={`col-span-2 lg:col-span-1 space-y-6 content-center ${
+              sidebarOpen ? "lg:col-span-2 xl:col-span-1" : ""
+            }`}
+          >
             <h2 className="text-3xl font-semibold mb-6 text-gray-200 tracking-wide">
               so... who am i?
             </h2>
@@ -46,14 +59,18 @@ export function AboutSection() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pl-4">
+          <div
+            className={`col-span-2 grid grid-cols-2 lg:col-span-1 gap-4 lg:pl-4 ${
+              sidebarOpen ? "lg:col-span-2 xl:col-span-1 lg:pl-0" : ""
+            }`}
+          >
             {values.map((value, index) => (
               <div
                 key={index}
-                className="transition-colors p-4 bg-grey rounded-lg border border-grey-l hover:border-blue-400"
+                className={`transition-colors p-4 bg-grey rounded-lg border border-grey-l hover:border-blue-400`}
               >
                 <div className="mb-3">{value.icon}</div>
-                <h3 className="font-semibold text-gray-200 mb-1">
+                <h3 className={`font-semibold text-${value.color} mb-1`}>
                   {value.title}
                 </h3>
                 <p className="text-base text-gray-400">{value.description}</p>
