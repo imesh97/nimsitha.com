a92ae35654f936f90a6f17d00122890b6327c957
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-02-16 10:04:15 -0500

    created initial skills page with json output

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,3 +1,44 @@
+import { SKILLS, Skill } from "@/constants";
+
+function formatSkillsData(skills: Skill[]) {
+  const categories = [...new Set(skills.map((skill) => skill.category))];
+
+  const newSkills = categories.map((category) => ({
+    category,
+    skills: skills
+      .filter((skill) => skill.category === category)
+      .map((skill) => skill.name),
+  }));
+
+  return newSkills;
+}
+
+function formatSkillsOutput(skills: { category: string; skills: string[] }[]) {
+  return (
+    "var MY_SKILLS = [\n" +
+    skills
+      .map(
+        (categoryGroup) =>
+          "  {\n" +
+          `    category: "${categoryGroup.category}",\n` +
+          "    skills: [\n" +
+          categoryGroup.skills.map((skill) => `      "${skill}"`).join(",\n") +
+          "\n" +
+          "    ]\n" +
+          "  }"
+      )
+      .join(",\n") +
+    "\n]"
+  );
+}
+
 export default function Skills() {
-  return <div>Still working on this...</div>;
+  const skills = formatSkillsData(SKILLS);
+  const output = formatSkillsOutput(skills);
+
+  return (
+    <div className="whitespace-pre-wrap font-code">
+      <p>{output}</p>
+    </div>
+  );
 }
