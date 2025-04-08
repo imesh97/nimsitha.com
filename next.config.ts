42235891a059463b5890be3de84859b4c94cf99f
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-04-08 18:04:26 -0400

    upgrade nextjs - avoid security vulnerability

--- next.config.ts
+++ next.config.ts
@@ -1,59 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: "webassembly/async",
-    });
-
-    return config;
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*.wasm",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/wasm",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-        ],
-      },
-      {
-        source: "/:path*.js",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/javascript",
-          },
-        ],
-      },
-      {
-        source: "/:path*.data",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/octet-stream",
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
